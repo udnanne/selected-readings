c98e66a62e9541d10ad8ec8920a75b5753ae9233
Anne Lin <annelin27@gmail.com>
2024-08-21 16:46:42 +0800

    chore: adjust for CLS

--- src/components/SliderBanner.jsx
+++ src/components/SliderBanner.jsx
@@ -9,7 +9,7 @@ function bgImgStyle(src) {
 export default function SliderBanner() {
     return (
         <Splide
-            className="splide-banner full-container" aria-label="橫幅"
+            className="splide-banner" aria-label="橫幅"
             options={{
                 type: "loop",
                 breakpoints: {
@@ -20,38 +20,45 @@ export default function SliderBanner() {
             }}
         >
             <SplideSlide>
-                <img className="splide__img" src="https://placehold.co/1280x600" alt="首頁大圖1" width="1280" height="600" fetchpriority="high" />
-                <div className="splide__bg" style={bgImgStyle("https://placehold.co/1280x600")}></div>
+                <img className="splide__img" src="https://placehold.co/1280x600" alt="首頁大圖1" width="1280" height="600" loading="lazy" fetchpriority="high" />
+                {/*<div className="splide__bg" style={bgImgStyle("https://placehold.co/1280x600")}></div>*/}
+                <img className="splide__bg" src="https://placehold.co/1280x600" alt="首頁背景1" width="1920" height="600" loading="lazy" />
             </SplideSlide>
 
             <SplideSlide>
-                <img className="splide__img" src="https://placehold.co/1500x600" alt="首頁大圖2" width="1280" height="600" fetchpriority="high" />
-                <div className="splide__bg" style={bgImgStyle("https://placehold.co/1500x600")}></div>
+                <img className="splide__img" src="https://placehold.co/1500x600" alt="首頁大圖2" width="1280" height="600" loading="lazy" />
+                {/*<div className="splide__bg" style={bgImgStyle("https://placehold.co/1500x600")}></div>*/}
+                <img className="splide__bg" src="https://placehold.co/1280x600" alt="首頁背景2" width="1920" height="600" loading="lazy" />
             </SplideSlide>
 
             <SplideSlide>
-                <img className="splide__img" src="https://placehold.co/1280x800" alt="首頁大圖3" width="1280" height="600" fetchpriority="high" />
-                <div className="splide__bg" style={bgImgStyle("https://placehold.co/1280x800")}></div>
+                <img className="splide__img" src="https://placehold.co/1280x800" alt="首頁大圖3" width="1280" height="600" loading="lazy" />
+                {/*<div className="splide__bg" style={bgImgStyle("https://placehold.co/1280x800")}></div>*/}
+                <img className="splide__bg" src="https://placehold.co/1280x600" alt="首頁背景3" width="1920" height="600" loading="lazy" />
             </SplideSlide>
 
             <SplideSlide>
-                <img className="splide__img" src="https://placehold.co/1500x800" alt="首頁大圖4" width="1280" height="600" fetchpriority="high" />
-                <div className="splide__bg" style={bgImgStyle("https://placehold.co/1500x800")}></div>
+                <img className="splide__img" src="https://placehold.co/1500x800" alt="首頁大圖4" width="1280" height="600" loading="lazy" />
+                {/*<div className="splide__bg" style={bgImgStyle("https://placehold.co/1500x800")}></div>*/}
+                <img className="splide__bg" src="https://placehold.co/1280x600" alt="首頁背景4" width="1920" height="600" loading="lazy" />
             </SplideSlide>
 
             <SplideSlide>
-                <img className="splide__img" src="https://placehold.co/1080x600" alt="首頁大圖5" width="1280" height="600" fetchpriority="high" />
-                <div className="splide__bg" style={bgImgStyle("https://placehold.co/1080x600")}></div>
+                <img className="splide__img" src="https://placehold.co/1080x600" alt="首頁大圖5" width="1280" height="600" loading="lazy" />
+                {/*<div className="splide__bg" style={bgImgStyle("https://placehold.co/1080x600")}></div>*/}
+                <img className="splide__bg" src="https://placehold.co/1280x600" alt="首頁背景5" width="1920" height="600" loading="lazy" />
             </SplideSlide>
 
             <SplideSlide>
-                <img className="splide__img" src="https://placehold.co/1280x400" alt="首頁大圖6" width="1280" height="600" fetchpriority="high" />
-                <div className="splide__bg" style={bgImgStyle("https://placehold.co/1280x400")}></div>
+                <img className="splide__img" src="https://placehold.co/1280x400" alt="首頁大圖6" width="1280" height="600" loading="lazy" />
+                {/*<div className="splide__bg" style={bgImgStyle("https://placehold.co/1280x400")}></div>*/}
+                <img className="splide__bg" src="https://placehold.co/1280x600" alt="首頁背景6" width="1920" height="600" loading="lazy" />
             </SplideSlide>
 
             <SplideSlide>
-                <img className="splide__img" src="https://placehold.co/1080x400" alt="首頁大圖7" width="1280" height="600" fetchpriority="high" />
-                <div className="splide__bg" style={bgImgStyle("https://placehold.co/1080x400")}></div>
+                <img className="splide__img" src="https://placehold.co/1080x400" alt="首頁大圖7" width="1280" height="600" loading="lazy" />
+                {/*<div className="splide__bg" style={bgImgStyle("https://placehold.co/1080x400")}></div>*/}
+                <img className="splide__bg" src="https://placehold.co/1280x600" alt="首頁背景7" width="1920" height="600" loading="lazy" />
             </SplideSlide>
         </Splide>
     );
