22a981f17a2e3f13e0addec6bbb7c9a83b974d9e
Anne Lin <annelin27@gmail.com>
2025-04-21 16:46:41 +0800

    feat: add dynamic filter title display

---
+++ src/components/FillterSearch.jsx
@@ -0,0 +1,12 @@
+export default function FillterSearch(props) {
+    const url = new URL(window.location.href);
+    const type = url.searchParams.get('type');
+
+    const isString = typeof props.search === "string" ? true : false;
+
+    if (isString) {
+        return props.search;
+    } else {
+        return props.search.find(item => item.url.includes(`type=${type}`))?.title || props.search[0].title
+    }
+}
